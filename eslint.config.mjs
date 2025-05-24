da0a87e49a6dd8db75a446521801eb24e6180b84
felipekgouvea <felipekinupesg@hotmail.com>
2025-05-24 14:12:14 -0300

    chore: disable simpleImportSort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
+// import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -12,15 +12,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-    "simple-import-sort": simpleImportSort,
-  },
-  rules: {
-    "simple-import-sort/imports" : "error",
-    "simple-import-sort/exports" : "error",
-  }
-  }
+  // {
+  //   plugins: {
+  //   "simple-import-sort": simpleImportSort,
+  // },
+  // rules: {
+  //   "simple-import-sort/imports" : "error",
+  //   "simple-import-sort/exports" : "error",
+  // }
+  // }
 ];
 
 export default eslintConfig;
