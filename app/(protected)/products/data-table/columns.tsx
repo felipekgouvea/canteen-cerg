a320f4f2f507f8b58af711e2424a4cc6961e3579
felipekgouvea <felipekinupesg@hotmail.com>
2025-06-02 20:13:33 -0300

    feat: add datatable on products page and include new dependency @tanstack/react-table

---
+++ app/(protected)/products/data-table/columns.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { formatCurrency } from "@/helpers/format-currency";
+import Image from "next/image";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/app/_components/ui/button";
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  menuCategory: {
+    name: string;
+  };
+  description: string;
+  imageUrl: string;
+};
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-sm font-semibold text-muted-foreground"
+        >
+          Nome do Produto
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{row.original.name}</div>;
+    },
+  },
+  {
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-sm font-semibold text-muted-foreground"
+        >
+          Preço do Produto
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{formatCurrency(row.original.price / 100)}</div>;
+    },
+  },
+  {
+    accessorKey: "menuCategory",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-sm font-semibold text-muted-foreground"
+        >
+          Categoria
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div>{row.original.menuCategory.name}</div>;
+    },
+  },
+
+  {
+    accessorKey: "imageUrl",
+    header: "Imagem",
+    cell: ({ row }) => {
+      return (
+        <div className="relative h-[65px] w-full bg-[#F2F2F2]">
+          <Image
+            src={row.original.imageUrl}
+            alt={row.original.name}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="rounded-lg object-cover"
+          />
+        </div>
+      );
+    },
+  },
+];
--- app/(protected)/products/page.tsx
+++ app/(protected)/products/page.tsx
@@ -11,13 +11,28 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import AddProductDialog from "./_components/add-product-dialog";
-
+import { DataTable } from "./data-table/data-table";
+import { columns } from "./data-table/columns";
+import { db } from "@/lib/prisma";
 const ProductsPage = async () => {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
     redirect("/authentication");
   }
+
+  const data = await db.product.findMany({
+    include: {
+      menuCategory: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   return (
     <PageContainer>
@@ -32,7 +47,7 @@ const ProductsPage = async () => {
       </PageHeader>
       <PageContent>
         <div>
-          <h1>Produtos</h1>
+          <DataTable columns={columns} data={data} pageSize={4} />
         </div>
       </PageContent>
     </PageContainer>
